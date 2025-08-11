bdc0137ed975df42fabf6a70eba03b6f7b6d7579
Debgandhar Ghosh <debg48@localhost.localdomain>
2025-08-11 07:43:52 +0530

    DB connected

--- initializers/database.go
+++ initializers/database.go
@@ -1,1 +1,23 @@
 package initializers
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectToDB() {
+	var err error
+	dsn := os.Getenv("DB_STRING")
+	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("DB connection failed")
+	} else {
+		fmt.Println(DB)
+	}
+}
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 func init() {
 	initializers.LoadEnvVaraibles()
+	initializers.ConnectToDB()
 }
 
 func main() {
