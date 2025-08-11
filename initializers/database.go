dc8fd87400ec62628ddd26071168f0dc0c11835c
Debgandhar Ghosh <debg48@localhost.localdomain>
2025-08-11 08:37:28 +0530

    fixed DB migration issue

--- initializers/database.go
+++ initializers/database.go
@@ -13,7 +13,10 @@ var DB *gorm.DB
 func ConnectToDB() {
 	var err error
 	dsn := os.Getenv("DB_STRING")
-	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if dsn == "" {
+    log.Fatal("Missing DB_STRING environment variable")
+	}
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("DB connection failed")
 	}
